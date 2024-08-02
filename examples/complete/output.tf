32d776be8a9c32ab4a04d7c57da9957ee4b36503
shanye <shanye.xzq@alibaba-inc.com>
2024-08-02 11:26:18 +0800

    examples: improve the examples

--- examples/complete/output.tf
+++ examples/complete/output.tf
@@ -2,10 +2,6 @@ output "rule_id" {
   value = module.example.rule_id
 }
 
-output "rule_status" {
-  value = module.example.rule_status
-}
-
 output "compliance_pack_id" {
   value = module.example.compliance_pack_id
 }
